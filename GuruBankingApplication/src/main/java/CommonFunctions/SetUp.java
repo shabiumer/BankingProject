285cb6393f4dd9e03012a84c439afb6e577c3c34
shabiumer <shabiumer@hotmail.com>
2018-09-05 12:21:33 +0100

    setup file updated as per requirement

--- GuruBankingApplication/src/main/java/CommonFunctions/SetUp.java
+++ GuruBankingApplication/src/main/java/CommonFunctions/SetUp.java
@@ -1,9 +1,16 @@
 package CommonFunctions;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -11,13 +18,29 @@ public class SetUp {
 	
 	public static WebDriver driver;
 	public static String url = "http://www.demo.guru99.com/V4/";
-	
+	public static String Browser = "firefox";
 	
 	@BeforeTest (alwaysRun=true)
 	public void openBrowser() {
 		
+		if (Browser == "chrome") {
+			System.setProperty("webdriver.chrome.driver", "C:\\Automation Jar files\\jar files\\chromedriver.exe");
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--user-data-dir=C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data\\Shoaib");
+			driver = new ChromeDriver(options);
+			
+		}
+		else {
 		System.setProperty("webdriver.gecko.driver","C:\\Automation Jar files\\jar files\\geckodriver.exe");
-		driver = new FirefoxDriver();
+		
+		DesiredCapabilities dc=DesiredCapabilities.firefox();
+		ProfilesIni profile = new ProfilesIni();
+		FirefoxProfile myprofile = profile.getProfile("shabi");
+		dc.setCapability(FirefoxDriver.PROFILE, myprofile);
+		driver = new FirefoxDriver(dc);
+		
+		}
 		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -26,6 +49,7 @@ public class SetUp {
 		driver.get(url);
 	}
 	
+
 	@AfterTest(alwaysRun=true)
 	public void closeBrowser() {
 
